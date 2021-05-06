ade2496fc2dd2aaca5e10a358c880a4faa9b74b6
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2021-05-06 12:54:14 +0800

    --

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 自用的Dwm配置
 ## 说明
-可以去我的Blog查看 -> [传送](https://re-blog.tk/ArchLinux/3c4647cb)
+可以去我的Blog查看 -> [传送](https://blog.rewrite.cf/ArchLinux/3c4647cb)
--- scripts/live-wallpaper.sh
+++ scripts/live-wallpaper.sh
@@ -12,18 +12,20 @@ nohup xwinwrap -ni -s -st -sp -b -nf -ov -- mpv --volume=0 --input-ipc-server=/t
 two(){
 # 双屏
 # left
+
 # mplayer
-#nohup xwinwrap -g 1920x1080+0+0 -ni -s -st -sp -b -nf -ov -- mplayer -af volume=-200 -softvol -softvol-max 200 -slave -quiet -input file=/tmp/wallpaper -loop 0 -wid WID -nolirc `find ~/Videos/live_wallpaper -type f` > /tmp/log/xwinwrap_left.log 2>&1 &
+nohup xwinwrap -g 1920x1080+0+0 -ni -s -st -sp -b -nf -ov -- mplayer -af volume=-200 -softvol -softvol-max 200 -slave -quiet -input file=/tmp/wallpaper -loop 0 -wid WID -nolirc `find ~/Videos/live_wallpaper -type f` > /tmp/log/xwinwrap_left.log 2>&1 &
 
 # mpv
-nohup xwinwrap -g 1920x1080+0+0 -ni -s -st -sp -b -nf -ov -- mpv --volume=0 --input-ipc-server=/tmp/wallpaper --idle -wid WID --shuffle ~/Videos/live_wallpaper > /tmp/log/xwinwrap_left.log 2>&1 &
+#nohup xwinwrap -g 1920x1080+0+0 -ni -s -st -sp -b -nf -ov -- mpv --volume=0 --input-ipc-server=/tmp/wallpaper --idle -wid WID --shuffle ~/Videos/live_wallpaper > /tmp/log/xwinwrap_left.log 2>&1 &
 
 # right
+
 # mplayer
-#nohup xwinwrap -g 1366x768+1920+0 -ni -s -st -sp -b -nf -ov -- mplayer -af volume=-200 -softvol -softvol-max 200 -slave -quiet -input file=/tmp/wallpaper -loop 0 -wid WID -nolirc `find ~/Videos/live_wallpaper -type f` > /tmp/log/xwinwrap_right.log 2>&1 &
+nohup xwinwrap -g 1366x768+1920+0 -ni -s -st -sp -b -nf -ov -- mplayer -af volume=-200 -softvol -softvol-max 200 -slave -quiet -input file=/tmp/wallpaper -loop 0 -wid WID -nolirc `find ~/Videos/live_wallpaper -type f` > /tmp/log/xwinwrap_right.log 2>&1 &
 
 # mpv
-nohup xwinwrap -g 1366x768+1920+0 -ni -s -st -sp -b -nf -ov -- mpv --volume=0 --input-ipc-server=/tmp/wallpaper --idle -wid WID --shuffle ~/Videos/live_wallpaper > /tmp/log/xwinwrap_right.log 2>&1 &
+#nohup xwinwrap -g 1366x768+1920+0 -ni -s -st -sp -b -nf -ov -- mpv --volume=0 --input-ipc-server=/tmp/wallpaper --idle -wid WID --shuffle ~/Videos/live_wallpaper > /tmp/log/xwinwrap_right.log 2>&1 &
 }
 
 if [ -z "$STA" ];then
